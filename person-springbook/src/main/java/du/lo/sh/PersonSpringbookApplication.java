162112b53556216b93c41278d0ec3bbe46ee0044
duanlongshan <duanlongshan@wilcom.com.cn>
2019-02-15 00:47:08 +0800

    import 用法

--- person-springbook/src/main/java/du/lo/sh/PersonSpringbookApplication.java
+++ person-springbook/src/main/java/du/lo/sh/PersonSpringbookApplication.java
@@ -1,10 +1,24 @@
 package du.lo.sh;
 
+import du.lo.sh.importcls.ImportClass;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+//@Import(ImportClass.class)
 public class PersonSpringbookApplication {
+
+	@Autowired
+	private ImportClass importClass;
+
+	@PostConstruct
+	public void init() {
+		System.out.println(importClass + "===");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonSpringbookApplication.class, args);
---
+++ person-springbook/src/main/java/du/lo/sh/importcls/ImportClass.java
@@ -0,0 +1,12 @@
+package du.lo.sh.importcls;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by duanlsh on 19/2/15.
+ */
+@Configuration
+public class ImportClass {
+
+
+}
