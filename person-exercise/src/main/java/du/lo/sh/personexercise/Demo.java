766985fa4ae6e6a8dbd65e828704333c10c5ffba
duanlsh <duan_longshan@126.com>
2019-02-15 16:03:17 +0800

    新增练习

---
+++ person-exercise/src/main/java/du/lo/sh/personexercise/Demo.java
@@ -0,0 +1,15 @@
+package du.lo.sh.personexercise;
+
+/**
+ * @ClassName Demo
+ * @Description TODO
+ * @Author duanlsh
+ * @Date 2019/2/15 16:01
+ * @Version 1.0
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+
+    }
+}
